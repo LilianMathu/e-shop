4e42e69f5a6394e436faeefc3c7512689544aa48
Lily Mathu <lilianmathu2018@gmail.com>
2020-05-23 04:15:55 +0300

    uploading an image

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+
+// Storage of files
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {      //func that def where incoming files should be stored
+        //callback is executed last. pass a potential error and relative path for storing the files
+        cb(null, './uploads/');
+    },
+    filename: function (req, file, cb) {       //func define how files should be named
+        //you can choose to give the original or the filename
+        cb(null, new Date().toISOString() + file.originalname);
+    }
+});
+
+//Specify the Mimetype by rejecting or accepting the mimetype
+const fileFilter = (req, file, cb) => {
+    //Accepting files
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image.png') {
+        cb(null, true);
+    }else {
+        //rejecting a file syntax
+        cb(null, false);
+    }
+
+};
+
+// Initialize multer
+// limit sets the size of the files acceptable in bytes. this case takes 5mb
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 1024 * 1024 * 10 },
+    fileFilter: fileFilter
+}); // upload is not publicly accessible Hence its saved in static files
+
 
 // Import the database models
 const Product = require('../models/Products');
@@ -8,30 +42,31 @@ const Product = require('../models/Products');
 // the routes
 
 // POST route
-router.post('/', (req, res) => {
+router.post('/', upload.single('productImage'), (req, res) => {
+    console.log(req.file);
     const product = new Product({
         name: req.body.name,
         price: req.body.price
     });
     product.save()
-    .then(product => {
-        res.status(201).json({
-            message: "created successfully",
-            newProduct: {
-                name: product.name,
-                price: product.price,
-                _id: product._id,
-                request: {
-                    type: 'GET',
-                    url: 'http://localhost:5000/products' + product._id
+        .then(product => {
+            res.status(201).json({
+                message: "created successfully",
+                newProduct: {
+                    name: product.name,
+                    price: product.price,
+                    _id: product._id,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:5000/products' + product._id
+                    }
                 }
-            }
+            });
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ error });
         });
-    })
-    .catch(error => {
-        console.log(error);
-        res.status(500).json({ error });
-    });
 });
 
 //GET route
@@ -42,14 +77,14 @@ router.get('/', (req, res) => {
         .then(products => {
             const response = {
                 count: products.length,
-                products:products.map(products => {
+                products: products.map(products => {
                     return {
                         name: products.name,
                         price: products.price,
                         _id: products._id,
                         request: {
                             type: 'GET',
-                            url: 'http://localhost:5000/products/'  + products._id
+                            url: 'http://localhost:5000/products/' + products._id
                         }
                     }
                 })
@@ -105,12 +140,12 @@ router.patch('/:productId', (req, res) => {
                 message: "successfully updated",
                 return: {
                     _id: product._id,
-                        name: product.name,
-                        price: product.price,
-                        request: {
-                            type: 'GET',
-                            url: 'http://localhost:5000/products' + product._id
-                        }
+                    name: product.name,
+                    price: product.price,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:5000/products' + product._id
+                    }
                 }
             });
         })
@@ -123,13 +158,13 @@ router.patch('/:productId', (req, res) => {
 // DELETE route
 router.delete('/:productId', (req, res) => {
     const id = req.params.productId;
-    Product.remove({_id: id})
-    .exec()
-    .then(product => {
-        res.status(200).json(product)
-    })
-    .catch(error => {
-        res.status(500).json({ error })
-    });
+    Product.remove({ _id: id })
+        .exec()
+        .then(product => {
+            res.status(200).json(product)
+        })
+        .catch(error => {
+            res.status(500).json({ error })
+        });
 });
 module.exports = router;
