65cd334cdb270ebfcae8700b3c1fb422fa5cf8f7
Lilian Mathu <lilianmathu2018@gmail.com>
2020-05-27 01:19:30 +0300

    setup user routes

---
+++ api/routes/user.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router;
+const mongoose = require('mongoose');
+
+// import models
+const userModel = require('../models/User');
+
+// Route
+router.post('/signup', (Req, res) => {
+
+});
