3939da0ee1d4dbf7d43c70b0e245991cfe3d9e2e
Lilian Mathu <lilianmathu2018@gmail.com>
2020-05-27 01:23:16 +0300

    setup user routes

--- api/routes/user.js
+++ api/routes/user.js
@@ -7,5 +7,8 @@ const userModel = require('../models/User');
 
 // Route
 router.post('/signup', (Req, res) => {
-
+    const user = new userModel({
+        email: req.body.email,
+        password: req.body.password
+    });
 });
