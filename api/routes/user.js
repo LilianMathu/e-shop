76de401e07bc9b1c45a209a97becf972be67bf37
Lilian Mathu <lilianmathu2018@gmail.com>
2020-05-27 13:49:03 +0300

    setup user routes

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const router = express.Router;
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 // import models
-const userModel = require('../models/User');
+const User = require('../models/User');
 
 // Route
-router.post('/signup', (Req, res) => {
-    const user = new userModel({
-        email: req.body.email,
-        password: req.body.password
-    });
+router.post('/signup', (req, res) => {
+    bcrypt.hash(req.body.email, 10, (err, hash) => {
+        if(err) {
+            res.status(500).json({ err });
+        } else {
+            const user = new User({
+                email: req.body.email,
+                password: hash
+            });
+
+            user.save()
+            .then(user => {
+                res.status(201).json({
+                    message: "User created",
+                    user
+                });
+            })
+            .catch(error => {
+                res.status(500).json({ error });
+            })
+        }
+    })
+
 });
